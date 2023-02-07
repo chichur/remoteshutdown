18ead18ca50ea09c88f1bebd26142b0039c4bfd9
chichur <thexbxeh@gmail.com>
2023-02-07 19:22:53 +0300

    Update INSTALL.bat

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,8 +1,8 @@
 chcp 65001
 cd "%~dp0"
 set app="%cd%\app.exe"
-nssm install "ShutdownService" "%app%"
 netsh advfirewall firewall add rule name="Shutdown Service Rule" dir=in action=allow program="%app%" enable=yes
+nssm install "ShutdownService" "%app%"
 nssm start "ShutdownService"
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
 echo & echo.& echo.& echo.Введите в строке браузера с любого устройства подключенного к сети:& echo.###########################& echo.#   %NetworkIP%   #& echo.###########################& echo.для отключение компьютера
